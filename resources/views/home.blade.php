7898a754200893f2c688210ae7145390c64a8a6d
unknown <richardasmarakandi@gmail.com>
2022-03-04 05:29:45 +0700

    Added Visi Misi

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -82,8 +82,8 @@
 			
     <div class="container background1 my-2 py-5" style="border-radius: 2.5rem">
         <div class="container py-3">
-            <div class="card card-body mx-auto" style="width:25%; border-radius: 2.5rem">
-                <img class="" style="" src="image/logokabinet.png" alt="">
+            <div class="card card-body mx-auto" style="width:25%; border-radius: 2.5rem;">
+                <img class="" src="image/logokabinet.png" alt="">
             </div>
         </div>
         <div class="container py-3">
@@ -97,7 +97,7 @@
         <div class="card card-body mx-auto" style="width: 55%;background-color:#f6c8a2;box-shadow: 5px 5px #eaaa79;">
             <h1 class="fw-bolder py-2 text-center" style="font-size:35px;">Tentang Kami</h1>
         </div>
-        <div class="my-5 d-flex justify-content-center" style="margin-left: 2px;margin-right: 2px;">
+        <div class="my-  d-flex justify-content-center" style="margin-left: 2px;margin-right: 2px;">
             <div class="row">
                 <div class="col d-flex justify-content-center">
                     <img class="" style="border-radius:80px;" src="image/gambar.png" alt="">
@@ -106,6 +106,69 @@
                     <h3>Lorem ipsum dolor sit amet consectetur adipisicing elit. Quam, quis et? Nobis dignissimos perferendis, temporibus porro enim maiores amet fuga provident asperiores praesentium necessitatibus minus, aperiam itaque quo placeat quos. Lorem ipsum dolor sit amet consectetur adipisicing elit. Laudantium natus consectetur magni atque perspiciatis ut harum sit cum ipsam molestiae quia, eos beatae et itaque fugit molestias ea rem velit?</h3>
                 </div>
             </div>
+        </div>
+    </div>
+
+    <div class="container my-5 py-5">
+        <div class="row">
+            <div class="col">
+                <div class="card card-body" style="border-radius:25px">
+                    <div class="mx-auto card card-body" style="width: 55%;background-color:#f6c8a2;box-shadow: 5px 5px #eaaa79;">
+                        <h1 class="fw-bolder py-2 text-center" style="font-size:35px;">Visi</h1>
+                    </div>
+                    <div class="d-flex justify-content-center my-3" style="margin-left: 2px;margin-right: 2px;">
+                        <div class="row">
+                            <div class="row col align-items-center">
+                                <h3 class="text-center">Lorem ipsum dolor sit amet consectetur adipisicing elit. Quam, quis et? Nobis dignissimos perferendis, temporibus porro enim maiores amet fuga provident asperiores praesentium necessitatibus minus, aperiam itaque quo placeat quos. Lorem ipsum dolor sit amet consectetur adipisicing elit. Laudantium natus consectetur magni atque perspiciatis ut harum sit cum ipsam molestiae quia, eos beatae et itaque fugit molestias ea rem velit?</h3>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+            <div class="col">
+                <div class="card card-body" style="border-radius:25px">
+                    <div class="mx-auto card card-body" style="width: 55%;background-color:#f6c8a2;box-shadow: 5px 5px #eaaa79;">
+                        <h1 class="fw-bolder py-2 text-center" style="font-size:35px;">Misi</h1>
+                    </div>
+                    <div class="d-flex justify-content-center my-3" style="margin-left: 2px;margin-right: 2px;">
+                        <div class="row">
+                            <div class="row col align-items-center">
+                                <h3 class="text-center">Lorem ipsum dolor sit amet consectetur adipisicing elit. Quam, quis et? Nobis dignissimos perferendis, temporibus porro enim maiores amet fuga provident asperiores praesentium necessitatibus minus, aperiam itaque quo placeat quos. Lorem ipsum dolor sit amet consectetur adipisicing elit. Laudantium natus consectetur magni atque perspiciatis ut harum sit cum ipsam molestiae quia, eos beatae et itaque fugit molestias ea rem velit?</h3>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+
+        <div class="card card-body mt-5 mb-3 mx-auto" style="width: 55%;background-color:#f6c8a2;box-shadow: 5px 5px #eaaa79;">
+            <h1 class="fw-bolder py-2 text-center" style="font-size:35px;">Kegiatan FOSIF</h1>
+        </div>
+        <div id="carouselExampleIndicators" class="carousel slide mx-auto" data-bs-ride="carousel" style="width:80%;border-style:double;border-width:20px;border-color:#eaaa79;border-radius:20px">
+            <div class="carousel-indicators">
+                <button type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide-to="0" class="active" aria-current="true" aria-label="Slide 1"></button>
+                <button type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide-to="1" aria-label="Slide 2"></button>
+                <button type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide-to="2" aria-label="Slide 3"></button>
+            </div>
+            <div class="carousel-inner" style="border-radius:20px;">
+                <div class="carousel-item active">
+                <img src="image/foto1.png" class="d-block w-100" alt="...">
+                </div>
+                <div class="carousel-item">
+                <img src="image/foto2.png" class="d-block w-100" alt="...">
+                </div>
+                <div class="carousel-item">
+                <img src="image/foto3.png" class="d-block w-100" alt="...">
+                </div>
+            </div>
+            <button class="carousel-control-prev" type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide="prev">
+                <span class="carousel-control-prev-icon" aria-hidden="true"></span>
+                <span class="visually-hidden">Previous</span>
+            </button>
+            <button class="carousel-control-next" type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide="next">
+                <span class="carousel-control-next-icon" aria-hidden="true"></span>
+                <span class="visually-hidden">Next</span>
+            </button>
         </div>
     </div>
 </body>
